e1e38044242b8ea7332b7bd90ddb8be263db6b63
Colin <cwrmcolin@gmail.com>
2024-11-05 13:27:44 -0800

    dev: checkpoint - configured basic hello-world endpoint

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,4 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -7,9 +6,18 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "cloudflare",
+      providers: { cloudflare: "5.45.0" },
     };
   },
   async run() {
 
+    const honoRestApiService = new sst.cloudflare.Worker("HonoService", {
+      url: true,
+      handler: "index.ts",
+    });
+
+    return {
+      api: honoRestApiService.url,
+    };
   },
 });
